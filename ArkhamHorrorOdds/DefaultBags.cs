2904cb6e5765db0dcddf00e7f23ad9fde1e09cad
Adam Dyjur-Mclean <dyjura@gmail.com>
2021-07-08 15:23:16 -0600

    Added default tokens for Dunwich.

--- ArkhamHorrorOdds/DefaultBags.cs
+++ ArkhamHorrorOdds/DefaultBags.cs
@@ -12,6 +12,8 @@ namespace ArkhamHorrorOdds
         {
             if (campaign == 0)
                 return notzBags(difficulty);
+            else if (campaign == 1)
+                return dlBags(difficulty);
             return new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         }
 
@@ -23,7 +25,20 @@ namespace ArkhamHorrorOdds
                 return new int[] { 1, 2, 3, 2, 1, 1, 0, 0, 0, 0, 2, 1, 1, 0, 1, 1 };
             else if(difficulty == 2)
                 return new int[] { 0, 3, 2, 2, 2, 1, 1, 0, 0, 0, 2, 1, 1, 0, 1, 1 };
-            return new int[] { 0, 1, 2, 2, 2, 2, 1, 1, 0, 1, 2, 1, 1, 0, 1, 1 };
+            else
+                return new int[] { 0, 1, 2, 2, 2, 2, 1, 1, 0, 1, 2, 1, 1, 0, 1, 1 };
+        }
+
+        private static int[] dlBags(int difficulty)
+        {
+            if (difficulty == 0)
+                return new int[] { 2, 3, 3, 2, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 1, 1 };
+            else if (difficulty == 1)
+                return new int[] { 1, 2, 3, 2, 1, 1, 0, 0, 0, 0, 2, 1, 0, 0, 1, 1 };
+            else if (difficulty == 2)
+                return new int[] { 0, 3, 2, 2, 2, 1, 1, 0, 0, 0, 2, 1, 0, 0, 1, 1 };
+            else
+                return new int[] { 0, 1, 2, 2, 2, 2, 1, 1, 0, 1, 2, 0, 0, 0, 1, 1 };
         }
     }
 }
