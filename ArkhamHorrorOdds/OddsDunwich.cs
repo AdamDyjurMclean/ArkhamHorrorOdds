33db39aff6bd37c05df0cc4bc5e5d63d3a75b004
Adam Dyjur-Mclean <dyjura@gmail.com>
2021-07-20 09:49:27 -0600

    Scenario 2 for Dunwich finished.

--- ArkhamHorrorOdds/OddsDunwich.cs
+++ ArkhamHorrorOdds/OddsDunwich.cs
@@ -40,6 +40,8 @@ namespace ArkhamHorrorOdds
             }
             if (scenario == 0)
                 Extra(bag, difficulty, skill, bonus, test, extra);
+            else if(scenario == 1)
+                House(bag, difficulty, skill, bonus, test, extra);
             if (blessCurse != "")
                 result += $"\n {blessCurse}";
             return result;
@@ -75,6 +77,38 @@ namespace ArkhamHorrorOdds
                 string elder = Math.Round((bag[14] / totalTokens * 100), 2).ToString();
                 result += $"\n {elder}% for Elder Thing, unknown result.";
             }
+            return;
+        }
+        private static void House(Dictionary<int, int> bag, int difficulty, int skill, int bonus, int test, int extra)
+        {
+            if (difficulty == 0)
+            {
+                if (Math.Max(skill + bonus - 2, 0) >= test)
+                    winLoss[0] += bag[11];
+                else
+                    winLoss[1] += bag[11];
+            }
+            else
+            {
+                if (Math.Max(skill + bonus - 3, 0) >= test)
+                    winLoss[0] += bag[11];
+                else
+                    winLoss[1] += bag[11];
+            }
+            if (Math.Max(skill + bonus - 3, 0) >= test)
+                winLoss[0] += bag[12];
+            else
+                winLoss[1] += bag[12];
+            if (Math.Max(skill + bonus - 2, 0) >= test)
+                winLoss[0] += bag[13];
+            else
+                winLoss[1] += bag[13];
+            if (Math.Max(skill + bonus, 0) >= test)
+                winLoss[0] += bag[14];
+            else
+                winLoss[1] += bag[14];
+            chance = Math.Round(winLoss[0] / totalTokens * 100, 2);
+            result = chance.ToString() + "% to win.";
             return;
         }
     }
