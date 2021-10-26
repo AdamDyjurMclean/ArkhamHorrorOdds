f5a3fd613c6fff6b950a5861aca2ad261619fe00
Adam Dyjur-Mclean <dyjura@gmail.com>
2021-10-26 10:39:57 -0600

    Refactored calculating odds for basic numbers.

--- ArkhamHorrorOdds/OddsBag.cs
+++ ArkhamHorrorOdds/OddsBag.cs
@@ -8,55 +8,33 @@ namespace ArkhamHorrorOdds
 {
     public static class OddsBag
     {
-        public static double[] Numbers(Dictionary<int, int> bag, int skill, int test, int star)
+        static int skill = 0;
+        static int test = 0;
+        private static bool Check( int value)
+        {
+            if(Math.Max(skill + value, 0) >= test)
+                return true;
+            else
+                return false;
+        }
+        public static double[] Numbers(Dictionary<int, int> bag, int skillValue, int testValue, int star)
         {
             int wins = 0;
             int losses = 0;
-            if(Math.Max(skill + 1, 0) >= test)
-                wins += bag[1];
-            else
-                losses += bag[1];
-            if(Math.Max(skill, 0) >= test)
-                wins += bag[0];
-            else
-                losses += bag[0];
-            if(Math.Max(skill - 1, 0) >= test)
-                wins += bag[-1];
-            else
-                losses += bag[-1];
-            if(Math.Max(skill - 2, 0) >= test)
-                wins += bag[-2];
-            else
-                losses += bag[-2];
-            if(Math.Max(skill - 3, 0) >= test)
-                wins += bag[-3];
-            else
-                losses += bag[-3];
-            if(Math.Max(skill - 4, 0) >= test)
-                wins += bag[-4];
-            else
-                losses += bag[-4];
-            if(Math.Max(skill - 5, 0) >= test)
-                wins += bag[-5];
-            else
-                losses += bag[-5];
-            if(Math.Max(skill - 6, 0) >= test)
-                wins += bag[-6];
-            else
-                losses += bag[-6];
-            if(Math.Max(skill - 7, 0) >= test)
-                wins += bag[-7];
-            else
-                losses += bag[-7];
-            if(Math.Max(skill - 8, 0) >= test)
-                wins += bag[-8];
-            else
-                losses += bag[-8];
-            if(Math.Max(skill + star, 0) >= test)
+            skill = skillValue;
+            test = testValue;
+            for(int i = -8; i <= 1; i++)
+            {
+                if (Check(i))
+                    wins += bag[i];
+                else
+                    losses += bag[i];
+            }
+            if(Check(star))
                 wins += bag[16];
             else
                 losses += bag[16];
-            return new double[]{ wins, losses };
+            return new double[] { wins, losses };
         }
     }
 }
