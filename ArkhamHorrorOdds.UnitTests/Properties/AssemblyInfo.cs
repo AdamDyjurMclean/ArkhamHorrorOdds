ada1bd672b7995ee422111f0175c6bd987c494f7
Adam Dyjur-Mclean <dyjura@gmail.com>
2021-12-28 12:03:29 -0700

    Added some unit tests for when to bring up the additional information boxes.

---
+++ ArkhamHorrorOdds.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArkhamHorrorOdds.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArkhamHorrorOdds.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("717a0cbd-a384-43a3-90a3-5a41143066c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ArkhamHorrorOdds.UnitTests/VarriableCheckTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ArkhamHorrorOdds.UnitTests
+{
+    [TestClass]
+    public class VarriableCheckTests
+    {
+        [TestMethod]
+        public void Check_IsInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.Check(2, 0, 2);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Check_IsNotInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.Check(1, 1, 0);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void Check2_IsInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.Check2(3, 6, 2);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Check2_IsNotInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.Check2(4, 1, 2);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void Check3_IsInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.Check3(3, 0, 0);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Check3_IsNotInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.Check3(4, 1, 2);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void CheckP_IsInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.CheckP(3, 4, 0);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void CheckP_IsNotInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.CheckP(1, 1, 2);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void CheckFlood_IsInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.CheckFlood(6, 5, 0);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void CheckFlood_IsNotInArray_ReturnsTrue()
+        {
+            bool result = VarriableCheck.CheckFlood(2, 5, 2);
+            Assert.IsFalse(result);
+        }
+    }
+}
