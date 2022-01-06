2ebf1220f7c74c61c2fb829242a893f75311285a
Adam Dyjur-Mclean <dyjura@gmail.com>
2022-01-05 23:43:04 -0700

    Added unit tests for token number conversion.

---
+++ ArkhamHorrorOdds.UnitTests/TokenNumberTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ArkhamHorrorOdds.UnitTests
+{
+    [TestClass]
+    public class TokenNumberTests
+    {
+        [TestMethod]
+        public void One_IsCorrect()
+        {
+            int result = TokenNumber.Convert(0);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void Five_IsCorrect()
+        {
+            int result = TokenNumber.Convert(6);
+            Assert.AreEqual(-5, result);
+        }
+        [TestMethod]
+        public void Eight_IsCorrect()
+        {
+            int result = TokenNumber.Convert(9);
+            Assert.AreEqual(-8, result);
+        }
+    }
+}
